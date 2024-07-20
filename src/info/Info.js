557ef1abd4af19869a2e5a6bc03d5badd3d33ddc
Kiranmoy Saha <kiranmoy2002@gmail.com>
2024-07-21 00:32:06 +0530

    name change

--- src/info/Info.js
+++ src/info/Info.js
@@ -27,10 +27,10 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
+    firstName: "Kiranmoy",
+    lastName: "Saha",
+    initials: "KS", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a tech enthusiast!",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
